ec27564b37870a5cff937e866f3d52da384361c7
He Limin <chelimin@gmail.com>
2016-07-18 00:31:23 +0800

    add orders controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+  #has access to all of methods inside application_controller.rb file
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+end
